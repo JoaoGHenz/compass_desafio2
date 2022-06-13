369201434530b05c8217b8acce84d59a804323e2
João <joao.henz.pb@compasso.com.br>
2022-06-13 15:11:57 -0300

    Feat: Main card

---
+++ src/components/MainCard.vue
@@ -0,0 +1,23 @@
+<template>
+    <div>
+        <h1 class="title">Intern Sign Up</h1>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'MainCard'
+}
+</script>
+
+<style scoped>
+    .title {
+        display: flex;
+        border-radius: 20px;
+        width: 617px;
+        height: 763px;
+        background-color: white;
+    }
+
+
+</style>
